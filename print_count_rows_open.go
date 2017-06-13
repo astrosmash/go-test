c358c37952cb30ade41f63bdd7ed8afc28b7070a
alexander.astashov <aa@vi.ai>
2017-06-13 15:03:05 +0300

    add

---
+++ print_count_rows_open.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			stat, errstat := os.Stat(arg)
+			if errstat != nil {
+				fmt.Fprintf(os.Stderr, "dup2_stat: %v\n", errstat)
+				continue
+			} else {
+				fmt.Printf("File info: %s\n---------- \n", stat)
+			}
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
